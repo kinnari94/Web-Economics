0be3e80dd5a621b2f549f00bb7b0770ecd0e23fd
kinnari94 <kinnari94@gmail.com>
2017-04-13 22:57:48 +0100

    update gbrt.py

--- gbrt.py
+++ gbrt.py
@@ -11,7 +11,7 @@ testdata = gl.SFrame.read_csv('validation.csv', verbose=False)
 #data = train.csv
 #click is target
 
-model = gl.gradient_boosted_regression.create(data, target='click', features=[ 'slotheight', 'slotwidth' ,'adexchange'],max_iterations=20, validation_set=None)
+model = gl.boosted_trees_regression.create(data, target='click', features=[ 'slotheight', 'slotwidth' ,'advertiser','city','hour'],max_iterations=20, validation_set=None)
 
 
 #predict the PCTR values against the validation dataset,
@@ -34,6 +34,6 @@ newdata = testdata.add_column(values, name='PCTR_GBRT')
 
 
 #SAVING
-newdata.save('validationprediction.csv', format='csv')
+newdata.save('validationprediction1.csv', format='csv')
 
 
