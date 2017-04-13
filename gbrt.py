ffe9f1338e919ee18e367d0a8e363eaaec35614c
kinnari94 <kinnari94@gmail.com>
2017-04-13 22:24:54 +0100

    GBRT model

---
+++ gbrt.py
@@ -0,0 +1,39 @@
+#import graphlab module
+import graphlab as gl
+
+#read training data CSV to variable 'data'
+data = gl.SFrame.read_csv('train.csv', verbose=False)
+
+#read validation data CSV to variable 'testdata'
+testdata = gl.SFrame.read_csv('validation.csv', verbose=False)
+
+#create logisic regression classifier from chosen features
+#data = train.csv
+#click is target
+
+model = gl.gradient_boosted_regression.create(data, target='click', features=[ 'slotheight', 'slotwidth' ,'adexchange'],max_iterations=20, validation_set=None)
+
+
+#predict the PCTR values against the validation dataset,
+#testdata = validation.csv
+#values = predicted values
+
+values = model.predict(testdata)
+
+
+#test model prediction, prints first 5 rows
+print values.head(5)
+
+
+#add values as a column
+#values = values from the prediction
+#newdata is the new variable for this new Sframe
+#testdata is our validation dataset
+
+newdata = testdata.add_column(values, name='PCTR_GBRT')
+
+
+#SAVING
+newdata.save('validationprediction.csv', format='csv')
+
+
